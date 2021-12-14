fea8b173622ec29db8141a9e0233b3491af0c789
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-14 14:02:49 -0800

    Added: Vowel or Consonant Program

---
+++ src/com/bridgelabz/basiccoreprograms/VowelorConsonant.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.basiccoreprograms;
+import java.util.Scanner;
+public class VowelorConsonant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Vowels or Consonants program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the alphabet ");
+        char ch = scanner.next().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+	}
+}
+

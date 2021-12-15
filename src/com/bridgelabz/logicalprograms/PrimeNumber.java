4fe59d32daad71b5185f3a99014e4e30021adb7c
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-14 17:34:45 -0800

    Added: Prime Number Program

---
+++ src/com/bridgelabz/logicalprograms/PrimeNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.logicalprograms;
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number:");
+		int n = sc.nextInt();
+		int i,count = 0;
+		for (i = 1;i <= n;i++) {
+			if (n%i == 0)
+				count = count+1;
+		}
+		if (count == 2)
+			System.out.println(n +" "+ "is a PrimeNumber");
+		else
+			System.out.println(n + " " +"is a Not PrimeNumber");
+
+	}
+
+}
